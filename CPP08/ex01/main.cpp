4107f2ea5b7868c32029fcd2bfa80c10b7693e5e
Adam Murawski <amurawsk@c1s13d61.42wolfsburg.de>
2023-12-19 17:36:20 +0100

    Updated main for better readability of tests.

--- CPP08/ex01/main.cpp
+++ CPP08/ex01/main.cpp
@@ -9,15 +9,16 @@ int main()
 	{
 		try
 		{
-			span2.addNumber(i);
+			span2.addNumber(i * 3 % 5);
+			std::cout << "number added!" << std::endl;
 		}
 		catch (std::exception &e)
 		{
-			std::cerr << std::endl << e.what() << std::endl;
+			std::cerr << e.what() << std::endl<< std::endl;
 		}
 	}
 	std::cout << "shortest distance is " << span2.shortestSpan() << std::endl;
-	std::cout << "longest distance is " << span2.longestSpan() << std::endl;
+	std::cout << "longest distance is " << span2.longestSpan() << std::endl<< std::endl;
 
 	Span span1 = Span(20000);
 	int arr[20000];
@@ -25,7 +26,7 @@ int main()
 		arr[i] = i;
 	span1.addRange(arr, arr + 20000);
 	std::cout << "shortest span is " << span1.shortestSpan() << std::endl;
-	std::cout << "longest span is " << span1.longestSpan() << std::endl << std::endl;
+	std::cout << "longest span is " << span1.longestSpan() << std::endl;
 
 	return (0);
 }
